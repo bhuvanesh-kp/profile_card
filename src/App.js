66f5c04b9d91f7a4c859dfc2efa1ec903ffd6aa7
bhuvanesh-kp <bhuvaneshkpb@gmail.com>
2024-10-25 07:55:03 +0530

    profile_card completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+
+const skills = [
+  {id:1,
+    skill:"React",
+    emoji:"🥳"
+  },
+  {
+    id:2,
+    skill:"Machine Learning",
+    emoji:"🪙"
+  },
+  {
+    id:3,
+    skill:"Web development",
+    emoji:"💸"
+  },
+  {
+    id:4,
+    skill:"Deep Learning",
+    emoji:"✌️"
+  },
+  {
+    id:5,
+    skill:"AI",
+    emoji:"🦾"
+  }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className ="container">
+      <div>
+        <img src="image.jpg" alt="img"/>
+      </div>
+      <div className="info">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <b>bhuvanesh_kp</b>
+        <br/>
+        Hello I am react developer I am going to expore more in react,
+        I know Machine learning and deep learning and AI 
+        by fields of interest are NLP and computer vision I am facinated by its growth and potential</p>
+        
+        <div className="skill-list">{skills.map(value=>(<Dispaly value={value} key={value.id}></Dispaly>))}</div>
+
+      </div>
+    </div>
+  );
+}
+
+function Dispaly({value}){
+  return(
+    <div className="data">
+        {value.skill}
+        {value.emoji}
     </div>
   );
 }
